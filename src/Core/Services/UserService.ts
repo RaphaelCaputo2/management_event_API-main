5197b41e50d8f26b56331561d59b4062fa1f6b22
Raphael Caputo <69820803+RaphaelCaputo2@users.noreply.github.com>
2022-05-31 23:01:51 -0300

    refact: Add userService

---
+++ src/Core/Services/UserService.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from 'tsyringe';
+import User from '../../Domain/Infrastructure/entities/User';
+import IUserRepository from '../../Domain/Infrastructure/interfaceRepository/IUserRepository';
+import ValidateUser from '../Tools/validations/ValidateUser';
+
+@injectable()
+export default class UserService {
+  constructor(@inject('UserRepository') private userRepository: IUserRepository) {}
+
+  public async validateUsers(email: string): Promise<User> {
+    const validateUser = new ValidateUser(this.userRepository, email);
+    await validateUser.execute(email);
+    return;
+  }
+  public async create(data: Omit<User, 'id'>): Promise<User> {
+    const user = await this.userRepository.create(data);
+    return user;
+  }
+  public async list(): Promise<User[]> {
+    const userList = await this.userRepository.list();
+    return userList;
+  }
+  public async login(email: string): Promise<User> {
+    const user = await this.userRepository.login(email);
+    return user;
+  }
+}
